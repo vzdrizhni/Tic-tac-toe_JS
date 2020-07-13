098914c4fe950af30c13c4c08619482068595414
Alexandru <rammazzoti2000@gmail.com>
2020-07-13 14:32:18 +0200

    Webpack, jest conig

---
+++ src/testing/createPlayer.test.js
@@ -0,0 +1,7 @@
+// import { createPlayer } from '../index';
+//
+// test('It should be equla to analyze result', () => {
+//   const objOne = new createPlayer('Nikulay', 'X');
+//   const objTwo = new createPlayer('Capos', 'O');
+//   expect(objOne.name).toBe('Nikulay');
+// });
