44d4b72f13358fb33c9560a8e8c90bbc840b438c
vzdrizhni <vzdrizhni@gmail.com>
2020-07-13 20:43:49 +0500

    Assignvalue function uncomplete

--- src/testing/gameBoard.test.js
+++ src/testing/gameBoard.test.js
@@ -2,6 +2,18 @@ const { GameBoard } = require('./createPlayer');
 
 const gameboard = GameBoard.getGameBoard();
 
+const filledBoard = {
+  '#a1': '',
+  '#a2': 'O',
+  '#a3': 'X',
+  '#b1': '',
+  '#b2': 'O',
+  '#b3': 'X',
+  '#c1': '',
+  '#c2': 'O',
+  '#c3': '',
+};
+
 test('it should be equal to empty board', () => {
   expect(gameboard).toEqual(
     {
@@ -17,3 +29,23 @@ test('it should be equal to empty board', () => {
     },
   );
 });
+
+test('', ()=> {
+  expect
+})
+
+describe('assignValue function', () => {
+  // GameBoard.assignValue('#a1', '');
+  GameBoard.assignValue('#a2', 'O');
+  // GameBoard.assignValue('#a3','X');
+  // GameBoard.assignValue('#b1', '');
+  // GameBoard.assignValue('#b2', 'O');
+  // GameBoard.assignValue('#b3', 'X');
+  // GameBoard.assignValue('#c1', '');
+  // GameBoard.assignValue('#c2', 'O');
+  // GameBoard.assignValue('#c3', '');
+
+  test('gameboard should be equal to fillboar', () => {
+    expect(filledBoard).toEqual(gameboard);
+  })
+})
